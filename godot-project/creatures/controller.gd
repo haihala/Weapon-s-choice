5352327f0de1cd65406aac2ec8cf9bab43f73f48
Eero Häihälä <eero.haihala@gmail.com>
2025-01-18 14:16:38 +0200

    Characters can take damage

--- godot-project/creatures/controller.gd
+++ godot-project/creatures/controller.gd
@@ -4,8 +4,8 @@ class_name Controller
 var direction: Vector2
 var creature: Creature
 
-func bind(creature: Creature) -> void:
-	self.creature = creature
+func bind(bind_target: Creature) -> void:
+	self.creature = bind_target
 
 func control() -> void:
 	pass
--- godot-project/creatures/creature.gd
+++ godot-project/creatures/creature.gd
@@ -6,7 +6,10 @@ class_name Creature
 var ai: Controller
 var controller: Controller
 
-@export var speed = 100
+@export var speed = 300
+@export var health = 2
+
+var hit_knockback: Vector2
 
 func _ready():
 	ai = ai_controller.new()
@@ -14,6 +17,21 @@ func _ready():
 	controller.bind(self)
 
 func _process(_delta: float) -> void:
-	controller.control()
-	velocity = controller.direction * speed
+	if $HitTimer.is_stopped():
+		controller.control()
+		velocity = controller.direction * speed
+	else:
+		# Getting hit
+		# This way it slows down over the timer and is normalized 1-0
+		velocity = hit_knockback * $HitTimer.time_left / $HitTimer.wait_time
 	move_and_slide()
+
+func take_damage(amount: int, impact_point: Vector2) -> void:
+	health -= amount
+	hit_knockback = (position - impact_point).normalized() * 500 * amount
+	$HitTimer.start()
+	if health <= 0:
+		die()
+
+func die() -> void:
+	get_tree().queue_delete(self)
