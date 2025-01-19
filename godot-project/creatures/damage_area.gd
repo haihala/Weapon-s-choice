0abb9196cccfeb7f25d85ca63ec83b0d378b8692
Eero Häihälä <eero.haihala@gmail.com>
2025-01-19 11:16:47 +0200

    Add Gobo implementation

--- godot-project/creatures/damage_area.gd
+++ godot-project/creatures/damage_area.gd
@@ -1,9 +1,17 @@
-extends Area2D
+extends Node2D
 
 @export var damage: int
+var blacklist: Array[Node2D]
 
-func _on_body_entered(body: Node2D) -> void:
-	if body == self or body == get_parent():
+func _ready() -> void:
+	blacklist.push_back(self)
+	blacklist.push_back(get_parent())
+
+func register_owner(creature: Creature) -> void:
+	blacklist.push_back(creature)
+
+func on_hit(body: Node2D) -> void:
+	if body is not Creature or body in blacklist:
 		return
 	print(body)
 	body.take_damage(damage, global_position)
--- godot-project/creatures/states/melee_attack_state.gd
+++ godot-project/creatures/states/melee_attack_state.gd
@@ -2,6 +2,7 @@ extends State
 
 @export var hitbox_offset: float
 @export var hitbox_scene: PackedScene
+@export_range(1, 10) var hitbox_scale: float = 1.0
 @export var startup_frames: int
 @export var lunge_distance: float
 
@@ -19,6 +20,8 @@ func activate_hitbox() -> void:
 		hitbox.position = creature.facing_direction * hitbox_offset
 		hitbox.rotation = creature.facing_direction.angle()
 		hitbox.register_owner(creature)
+		hitbox.scale.x = hitbox_scale
+		hitbox.scale.y = hitbox_scale
 		creature.add_child(hitbox)
 	creature.force_movement(lunge_distance * creature.facing_direction, 0.5)
 	has_activated = true
